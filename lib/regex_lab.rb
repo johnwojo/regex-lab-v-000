8789fbe8ac0e3c52efd431bb15f38cfe063e7130
John Wojciechowski <jwojo13@gmail.com>
2017-11-21 19:28:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,6 +24,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\(*\d{3}\)*\s*\d{3}\s*\-*\d{4}\b/) ? true : false
+  phone.match(/\(*\d{3}\)*\s*\d{3}\s*\-*\d{4}\b/) ? true : false
   end
-end
+
